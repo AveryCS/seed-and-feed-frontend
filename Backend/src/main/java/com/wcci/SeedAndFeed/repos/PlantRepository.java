fb65fb9a4589cfb9b6c0b0f0c55eec2e79d2d2d0
AveryCS <Averydcs@gmail.com>
2023-01-02 17:30:18 -0500

    deleting the backend

--- Backend/src/main/java/com/wcci/SeedAndFeed/repos/PlantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wcci.SeedAndFeed.repos;
-
-import com.wcci.SeedAndFeed.entities.Plant;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlantRepository extends CrudRepository<Plant,Long> {
-}
